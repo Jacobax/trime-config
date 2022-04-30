8f8001cab48f938d2e507941a462acd80b71a966
Jacobax <2823442326@qq.com>
2022-04-30 12:03:14 +0800

    新

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 clover拼音+小鹤双拼+小鹤音形  
-![ios](https://raw.githubusercontent.com/Jacobax/rime4android-config/main/iOS%E7%9A%AE%E8%82%A4.jpg)  
-![Gboard](https://raw.githubusercontent.com/Jacobax/rime4android-config/main/配色Gboard墨.jpg)
+![ios](https://raw.githubusercontent.com/Jacobax/rime4android-config/main/pics/iOS%E7%9A%AE%E8%82%A4.jpg)  
+![Gboard](https://raw.githubusercontent.com/Jacobax/rime4android-config/main/pics/配色Gboard墨.jpg)
---
+++ rime.lua
@@ -0,0 +1,63 @@
+--- 过滤器：单字在先
+function single_char_first_filter(input)
+    local l = {}
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1) then
+            yield(cand)
+        else
+            table.insert(l, cand)
+        end
+    end
+    for cand in ipairs(l) do
+        yield(cand)
+    end
+end
+
+--- 计算器
+--- @KyleBing 2022-01-17
+
+function calculator(input, seg)
+    if string.find(input, 'coco') ~= nil then -- 匹配 coco 开头的字符串
+        local _, _, a, operation, b = string.find(input, "coco(%d+%.?%d*)([%+%-%*/])(%d+%.?%d*)")
+        local result = 0
+        if operation == '+' then
+            result = a + b
+        elseif operation == '-' then
+            result = a - b
+        elseif operation == '*' then
+            result = a * b
+        elseif operation == '/' then
+            result = a / b
+        end
+        yield(Candidate("coco", seg.start, seg._end, result, "计算器"))
+    end
+end
+
+
+function date_translator(input, seg)
+    if (input == "date") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+    end
+    if (input == "time") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分"), "  "))
+        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分%S秒"), "  "))
+    end
+
+    -- @JiandanDream
+    -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
+
+    if (input == "week") then
+        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+        yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
+    end
+end
